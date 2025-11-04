3240ff85c8ca14001b66a50d80d220867ed06a7f
Aiden B <156241225+791747hnlhymt@users.noreply.github.com>
2025-11-03 19:36:07 -0500

    added more to the ui
    i added left and right arrows to the ui but the js logic still needs work.

---
+++ Scripts/arrowButtonMap.js
@@ -0,0 +1,5 @@
+document.getElementById('leftArrowButton').addEventListener('click' , () => {
+    document.getElementById('shelfContainer').classList.add('hidden');
+    document.getElementById('leftArrowshelfContainer').classList.remove('hidden');
+});
+    
